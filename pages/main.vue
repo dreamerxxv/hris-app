fa069c65b26060f565ce06c8c8121eb358516a67
dreamerxxv <successentrepren@gmail.com>
2024-09-10 17:54:35 +0700

    add project

---
+++ pages/main.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'MainPage',
+}
+</script>
+
+<template>
+    <div>
+        Hello
+    </div>
+</template>
---
+++ pages/statistic.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'StatisticPage',
+}
+</script>
+
+<template>
+    <div>
+        Hello Statistic
+    </div>
+</template>
